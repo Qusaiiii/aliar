2aba615d2437f4f66f2429fed7dd55c6d7377d8c
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-09-24 22:11:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -318,6 +318,36 @@ function play(guild, song) {
 }
 });
 
+client.on('message', function (message) {
+    var messageParts = message.content.split(' ');
+
+    var command = messageParts[0].toLowerCase();
+    var parameters = messageParts.splice(1, messageParts.length);
+
+
+    switch (command) {
+        case "-join":
+        if(message.guild.voiceConnection){
+            message.reply('I\'m Already In A Voice Connection!');
+        }else if(!message.member.voiceChannel){
+            message.reply('You\'re Not In A Voice Channel!');
+        }else{
+    let channel = message.member.voiceChannel;
+    channel.join();
+        }
+            break;
+case "-44443play":
+        if(!message.guild.voiceConnection){
+            message.reply('I\'m Not In A Voice Channel!');
+        }else{
+//كود بدء الموسيقى مالك
+        }
+            var voiceConnection = client.voiceConnections.first();
+
+            break;
+}
+});
+
   client.on('message', message => {//help msg
   if (message.author.bot) return;
    if (message.content === prefix + "help") {
